c74b594ed129eba8f20a78d4c2df39179474668f
johir-rayhan <johir.rayhan01@gmail.com>
2020-08-20 15:11:01 +0600

    Show data

--- App.js
+++ App.js
@@ -1,13 +1,109 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Dimensions } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
 
-export default function App() {
+const { width, height } = Dimensions.get('screen')
+// https://www.creative-flyers.com
+const DATA = [
+  {
+    title: 'Afro vibes',
+    location: 'Mumbai, India',
+    date: 'Nov 17th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Afro-vibes-flyer-template.jpg',
+  },
+  {
+    title: 'Jungle Party',
+    location: 'Unknown',
+    date: 'Sept 3rd, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2019/11/Jungle-Party-Flyer-Template-1.jpg',
+  },
+  {
+    title: '4th Of July',
+    location: 'New York, USA',
+    date: 'Oct 11th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/4th-Of-July-Invitation.jpg',
+  },
+  {
+    title: 'Summer festival',
+    location: 'Bucharest, Romania',
+    date: 'Aug 17th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/07/Summer-Music-Festival-Poster.jpg',
+  },
+  {
+    title: 'BBQ with friends',
+    location: 'Prague, Czech Republic',
+    date: 'Sept 11th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/BBQ-Flyer-Psd-Template.jpg',
+  },
+  {
+    title: 'Festival music',
+    location: 'Berlin, Germany',
+    date: 'Apr 21th, 2021',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Festival-Music-PSD-Template.jpg',
+  },
+  {
+    title: 'Beach House',
+    location: 'Liboa, Portugal',
+    date: 'Aug 12th, 2020',
+    poster:
+      'https://www.creative-flyers.com/wp-content/uploads/2020/06/Summer-Beach-House-Flyer.jpg',
+  },
+];
+
+const OVERFLOW_HEIGHT = 70;
+const SPACING = 10;
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
+const VISIBLE_ITEMS = 3;
+
+const OverflowItems = ({ data }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.overflowContainer}>
+      <View>
+        {
+          data.map((item,index) => {
+            return (
+              <View key={index} sytle={styles.overflowItem}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {item.title}
+                </Text>
+                <View style={styles.itemContainer}>
+                  <Text style={styles.location}>
+                    <EvilIcons
+                      name='location'
+                      size={16}
+                      color='black'
+                      style={{ marginRight: 5 }}
+                      />
+                      {item.location}
+                  </Text>
+                  <Text style={styles.date}>
+                    {item.date}
+                  </Text>
+                </View>
+              </View>
+            )
+          })
+        }
+      </View>
     </View>
+  )
+}
+export default function App() {
+  const [ data, setData ] = useState(DATA)
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar hidden />
+      <OverflowItems data={data} />
+      <Text>Open up App.js to start working on your app!</Text>
+    </SafeAreaView>
   );
 }
 
